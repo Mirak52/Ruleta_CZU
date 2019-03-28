f321f92f17d0309fb779c765d08ab3d059044cab
Marek Bastl <marek.bastl5@gmail.com>
2019-03-28 21:08:33 +0100

    Init

---
+++ Ruleta_CZU/Ruleta_CZU/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Obecné informace o sestavení se řídí přes následující 
+' sadu atributů. Změnou hodnot těchto atributů se upraví informace
+' přidružené k sestavení.
+
+' Zkontrolujte hodnoty atributů sestavení.
+
+<Assembly: AssemblyTitle("Ruleta_CZU")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Ruleta_CZU")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+<Assembly: Guid("b9c756d8-e767-4988-9e28-bf63481fee95")>
+
+' Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+'
+'      Hlavní verze
+'      Podverze
+'      Číslo sestavení
+'      Revize
+'
+' Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize pomocí *, jak je vidět níže:
+' pomocí zástupného znaku * takto:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
